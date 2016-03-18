99b649739f715ab0f6a617722c34e9471dae6d7b
vazyvite <jonathan.jacques@capgemini.com>
2016-03-18 11:47:50 +0100

    passage en version 0.2.7

--- bat/copyStubbed.bat
+++ bat/copyStubbed.bat
@@ -29,3 +29,9 @@ echo.
 cd /D D:\
 cd "%root%%pathServices%\psl-services-ear\target"
 "C:\Program Files\7-Zip\7z.exe" a -tzip psl-services-ear.ear *.jar
+
+echo.
+echo +--------------------------------+
+echo *    Supprime Services Stubbed   *
+echo +--------------------------------+
+echo.
